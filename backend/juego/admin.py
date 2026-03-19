bf69411c8f4aa83149b1ef6df35b1c68287c3cbd
sergiopons-1 <sergioponslopez@gmail.com>
2026-03-19 19:51:39 +0100

    migraciones

--- backend/juego/admin.py
+++ backend/juego/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Minijuego, Progreso, Rehabilitacion, Edificio, Notas
+
 # Register your models here.
+admin.site.register(Minijuego)
+admin.site.register(Progreso)
+admin.site.register(Rehabilitacion)
+admin.site.register(Edificio)
+admin.site.register(Notas)
